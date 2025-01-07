70bd118b05e956a2f438351494b8543aeb9cd4c4
Kalmarr-dev <art.hydrogen1405@gmail.com>
2025-01-07 21:06:44 +0200

    fix: separate chat for testing into it's own HTML

--- app.ts
+++ app.ts
@@ -50,48 +50,12 @@ io.on("connection", (socket) => {
 });
 
 app.get("/chat", (_, res) => {
-  res.send(`<script src="/socket.io/socket.io.js"></script>
-<input type="text" id="chatInput">
-<script>
-  const socket = io(); // put URL as a parameter
-  socket.emit("ping", ()=>{console.log("pong delivered to server")});
-  socket.on("pong", ()=>{console.log("pong")});
-
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NzA0NDdhMGEwYmNiODQ3MGQ5MjhlZSIsImlhdCI6MTczNjE3NDc5MiwiZXhwIjoxNzM2MTc4MzkyfQ.fjLDIaLrbirX7Nyt0kVO4JnC_1ndd6nr-_gAxC5AJnU";
-
-  socket.emit("set chat rooms", token, (chats)=>{
-    console.log(chats);
-
-    const input = document.getElementById("chatInput");
-    input.onchange = () => {
-      console.log(input.value);
-      const message = document.createElement("p");
-      message.innerHTML = input.value;
-      document.body.insertBefore(message, document.body.lastElement);
-      socket.emit("chat message", token, { message: input.value, chat_id: chats[0]._id }, ()=>{
-        console.log("chat message delivered")
-      });
-    }
-
-    socket.emit(
-        "get messages before", 
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NzA0NDdhMGEwYmNiODQ3MGQ5MjhlZSIsImlhdCI6MTczNTQxMzAxMiwiZXhwIjoxNzM1NDE2NjEyfQ.rUjHAldTEAhwpSJO3GEx0BFfbwiTKN2cwHDB2NeQF7s", 
-        chats[0]._id, Date.now(), (messages)=>{console.log(messages)});
-  });
-
-
-  socket.on("chat message", (messageObject) => {
-    const message = document.createElement("p");
-    console.log(messageObject);
-    message.innerHTML = messageObject.name + ": " + messageObject.text;
-    message.style.color = "blue";
-    document.body.insertBefore(message, document.body.lastElement);
-  })
-</script>`);
+  res.sendFile("test_frontend/chat.html", {root: __dirname});
 });
 
 
 connectWithRetry(5, 5000).then(async () => {
+  // TEST message insertion
   await MessageModel.addTestMessage();
   server.listen(PORT, () => {
     console.log(`Server is running. Use our API on port: ${PORT}`);
